560b8dc916e0d32c4c42d4345751e995bcebbcf3
negtvozero-jpg <negtvozero@gmail.com>
2026-01-30 15:34:49 -0300

    Update overlay.js

--- overlay/overlay.js
+++ overlay/overlay.js
@@ -53,9 +53,7 @@ const CONFIG = {
   spawnSpread: 1,
   poolSize: 200
 };
-  
-window.VDAY = window.VDAY || {};
-window.VDAY.config = CONFIG;      
+
 
 function argbToHex(argb) {
   const rgb = (argb >>> 0) & 0x00ffffff;
